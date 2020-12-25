74a206536db1658ebfeac19b0c7a9723c14c4ad2
“Guikai <guikai_huang@outlook.com>
2020-12-25 13:39:47 +0800

    header navbar

---
+++ src/base/navbar/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mine-navbar">
+    <div class="mine-navbar-left" v-if="$slots.left">
+      <slot name="left"></slot>
+    </div>
+    <div class="mine-navbar-center" v-if="$slots.center">
+      <slot name="center"></slot>
+    </div>
+    <div class="mine-navbar-right" v-if="$slots.right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MeNavbar'
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/scss/mixin";
+  .mine-navbar{
+    @include flex-between();
+    height: 50px;
+    background-color: #fff;
+
+    &-left {
+      margin-left: 10px;
+    }
+
+    &-center {
+      margin: 0 10px;
+      flex: 1;
+    }
+
+    &-right {
+      margin-right: 10px;
+    }
+  }
+</style>
---
+++ src/pages/home/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <me-navbar class="header">
+    <i class="iconfont icon-richscan_icon" slot="left"></i>
+    <div slot="center">搜索框</div>
+    <i class="iconfont icon-xiaoxi" slot="right"></i>
+  </me-navbar>
+</template>
+
+<script>
+  import MeNavbar from 'base/navbar';
+  export default {
+    name: 'HomeHeader',
+    components: {
+      MeNavbar
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/scss/mixin";
+  .header {
+    &.mine-navbar{
+      // background-color: transparent;
+      background-color: $header-bgc-translucent;
+
+    }
+
+    .iconfont {
+      color: $icon-color-default;
+      font-size: $icon-font-size;
+    }
+  }
+</style>
