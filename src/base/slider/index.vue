ae7332935da3b5b08920ef6eb92b060106542a79
“Guikai <guikai_huang@outlook.com>
2020-12-26 23:54:02 +0800

    vue swiper

---
+++ src/base/slider/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <swiper :option="swiperOption">
+    <slot></slot>
+    <div class="swiper-pagination" v-if="pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+  import {swiper} from 'vue-awesome-swiper';
+  export default {
+    name: 'MeSlider',
+    components: {
+      swiper
+    },
+    props: {
+      direction: {
+        type: String,
+        default: 'horizontal',
+        validator(value) {
+          return [
+            'horizontal',
+            'vertical'
+          ].indexOf(value) > -1;
+        }
+      },
+      interval: {
+        type: Number,
+        default: 3000,
+        validator(value) {
+          return value > 0;
+        }
+      },
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      pagination: {
+        type: Boolean,
+        default: true
+      }
+    },
+    data() {
+      return {
+        swiperOption: {
+          watchOverflow: true,
+          direction: this.direction,
+          autoplay: this.interval ? {
+            dalay: this.interval,
+            disableOnInteraction: false
+          } : false,
+          slidesPerview: 1,
+          loop: this.loop,
+          pagination: {
+            el: this.pagination ? '.swiper-pagination' : null
+          }
+        }
+      };
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/scss/mixin";
+</style>
---
+++ src/pages/home/slider.vue
@@ -0,0 +1,55 @@
+<template>
+  <me-slider
+    :direction="direction"
+    :loop="loop"
+    :interval="interval"
+    :pagination="pagination"
+  >
+    <swiper-slide
+      v-for="(item, index) in sliders"
+      :key="index"
+    >
+      <a :href="item.linkUrl" class="slider-link">
+        <img :src="item.picUrl" alt="" class="slider-img">
+      </a>
+    </swiper-slide>
+  </me-slider>
+</template>
+
+<script>
+  import MeSlider from 'base/slider';
+  import {swiperSlide} from 'vue-awesome-swiper';
+  export default {
+    name: 'HomeSlider',
+    components: {
+      MeSlider,
+      swiperSlide
+    },
+    data() {
+      return {
+        sliders: [
+          {
+            'linkUrl': 'www.baidu.com',
+            'picUrl': require('./1.jpg')
+          },
+          {
+            'linkUrl': 'www.baidu.com',
+            'picUrl': require('./2.jpg')
+          },
+          {
+            'linkUrl': 'www.baidu.com',
+            'picUrl': require('./3.jpg')
+          },
+          {
+            'linkUrl': 'www.baidu.com',
+            'picUrl': require('./4.jpg')
+          }
+        ]
+      };
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/scss/mixin";
+</style>
